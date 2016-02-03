35459b0d3c50074c1365f2e59da9b8503cfc2334
JoyDore63 <joy.dore@isotoma.com>
2016-02-03 14:34:27 +0000

    added User and Category

--- expenses_website/expenses/models.py
+++ expenses_website/expenses/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+"""
+The User can create an Expense which will be in a category.
+For example,
+   User: Tony
+   Category: Treats
+   Expense: Coffee
+"""
+
+
+class User(models.Model):
+    name = models.CharField(max_length=6)
+
+
+class Category(models.Model):
+    description = models.CharField(max_length=30)
