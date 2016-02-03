fa35dc880bbe14fc6fc9a69f272aed3f1c8dc613
JoyDore63 <joy.dore@isotoma.com>
2016-02-03 16:09:05 +0000

    added __unicode__ methods and ordering after feedback from Alex2

--- expenses_website/expenses/models.py
+++ expenses_website/expenses/models.py
@@ -15,12 +15,22 @@ For example,
 class User(models.Model):
     name = models.CharField(max_length=6)
 
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+    class Meta:
+        ordering = ['name']
+
 
 class Category(models.Model):
     description = models.CharField(max_length=30)
 
+    def __unicode__(self):
+        return u"{}".format(self.description)
+
     class Meta:
         verbose_name_plural = 'Categories'  # Shows in admin site
+        ordering = ['description']
 
 
 class Expense(models.Model):
@@ -30,3 +40,18 @@ class Expense(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=2)
     # Include 'human-readable' name for purchase date
     purchase_date = models.DateTimeField('date of purchase')
+
+    def __unicode__(self):
+        #  \xa puts a pound sign in front of the price
+        return u"{} - {} - {} - \xa3{} - {}".format(
+            self.description,
+            self.user,
+            self.category,
+            self.price,
+            self.purchase_date
+        )
+
+    class Meta:
+        ordering = [
+            'user', 'category', 'purchase_date', 'price', 'description'
+        ]
