bd299f6ec09a534606723dbfce50e90752ba4603
JoyDore63 <joy.dore@isotoma.com>
2016-02-03 16:56:42 +0000

    Make the expense date just a date

--- expenses_website/expenses/models.py
+++ expenses_website/expenses/models.py
@@ -29,4 +29,4 @@ class Expense(models.Model):
     category = models.ForeignKey(Category)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     # Include 'human-readable' name for purchase date
-    purchase_date = models.DateTimeField('date of purchase')
+    purchase_date = models.DateField('date of purchase')
