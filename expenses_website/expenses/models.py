3073bf5aacd3b0dcf00bb208563c1ccff900eba9
JoyDore63 <joy.dore@isotoma.com>
2016-02-03 15:04:16 +0000

    added Expense class and migration

--- expenses_website/expenses/models.py
+++ expenses_website/expenses/models.py
@@ -18,3 +18,15 @@ class User(models.Model):
 
 class Category(models.Model):
     description = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = 'Categories'  # Shows in admin site
+
+
+class Expense(models.Model):
+    description = models.CharField(max_length=120)
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    # Include 'human-readable' name for purchase date
+    purchase_date = models.DateTimeField('date of purchase')
