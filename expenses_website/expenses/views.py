2cc380629ab76db57f0dad34a2abd064884d21ca
JoyDore63 <joy.dore@isotoma.com>
2016-02-04 10:14:59 +0000

    initial view

--- expenses_website/expenses/views.py
+++ expenses_website/expenses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Expense
+
+
+class IndexView(generic.ListView):
+    template_name = 'expenses/index.html'
+    context_object_name = 'recent_expenses_list'
+
+    def get_queryset(self):
+        '''Return the last 20 expenses'''
+        return Expense.objects.order_by('-purchase_date')[:20]
