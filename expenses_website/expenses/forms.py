8915c938bbe14ded47ad127a0cf8e42ede8b4ece
JoyDore63 <joy.dore@isotoma.com>
2016-02-19 10:10:27 +0000

    Dave's version of FilterListForm

--- expenses_website/expenses/forms.py
+++ expenses_website/expenses/forms.py
@@ -1,7 +1,11 @@
+import logging
+
 from django import forms
 from django.utils import timezone
 
 from .models import Category
+
+logger = logging.getLogger(__name__)
 
 
 class CreateExpenseForm(forms.Form):
@@ -16,15 +20,21 @@ class CreateExpenseForm(forms.Form):
 
 
 class FilterListForm(forms.Form):
+
     # Create a list of tuples as MultipleChoiceField is not happy with
     # list of objects from Category.objects.all()
     choice_list = []
-    for category in Category.objects.all():
-        choice_list.append((category.pk, category.description))
     # Get just the pks for initial values => all checkboxes will be ticked
-    initial_values = [pk for pk, description in choice_list]
+    initial_values = []
+
     category_choices = forms.MultipleChoiceField(
         choice_list,
         widget=forms.CheckboxSelectMultiple,
         initial=initial_values
     )
+
+    def __init__(self, **kwargs):
+        for category in Category.objects.all():
+            self.choice_list.append((category.pk, category.description))
+
+        self.initial_values = [pk for pk, description in self.choice_list]
