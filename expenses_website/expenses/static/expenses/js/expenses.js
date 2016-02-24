5f917e1f76796394f6af134df425bb5f89c1891d
JoyDore63 <joy.dore@isotoma.com>
2016-02-24 16:44:47 +0000

    stage 2, validation on click of create button

--- expenses_website/expenses/static/expenses/js/expenses.js
+++ expenses_website/expenses/static/expenses/js/expenses.js
@@ -1,12 +1,64 @@
 $(document).ready(function(){
-    // want to detect when might have entered some data
-    $("input, select").on({
-        focusout: function(){
-            $(this).css("background-color", "yellow");
-            // TODO make this conditional on validation
-            // also needs to only apply to the Create button,
-            // not other submit buttons on other pages
-            $("input[type=submit]").removeAttr('disabled');
+    $('#id_category').on('change', function(){
+        var input=$(this);
+        var value=input.val();
+        if (value){
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+        }
+    });
+    $('#id_description').on('input', function(){
+        var input=$(this);
+        var value=input.val();
+        if (value){
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+        }
+    });
+    $("#id_price").on('input', function() {
+        var input=$(this);
+        var value=input.val();
+        if (value){
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+        }
+    });
+    // taken from https://formden.com/blog/validate-contact-form-jquery
+    $("#expense_submit").click(function(event){
+        // Don't need to validate the date fields, they have defaults
+        $("#id_purchase_date_month").addClass("valid");
+        $("#id_purchase_date_day").addClass("valid");
+        $("#id_purchase_date_year").addClass("valid");
+        // Likewise CSRF token and all hidden fields should be valid
+        // This doesn't actually work for csrf token !! :-(
+        $("input:hidden").addClass("valid");
+
+        var form_data=$("#expense").serializeArray();
+        var error_free=true;
+        for (var input in form_data){
+            var element=$("#id_"+form_data[input]['name']);
+            // Ignore csrf token, all attempts at adding valid to it have failed
+            if (element.selector == '#id_csrfmiddlewaretoken'){continue;}
+            var valid=element.hasClass("valid");
+            var error_element=$("span", element.parent());
+            if (!valid){
+                error_element.removeClass("error").addClass("error_show");
+                error_free=false;
+            }
+            else{error_element.removeClass("error_show").addClass("error");}
+        }
+        if (!error_free){
+            event.preventDefault();
+            alert("Errors: Form will not be submitted")
+        }
+        else{
+            alert('No errors: Form will be submitted');
         }
     });
 });
