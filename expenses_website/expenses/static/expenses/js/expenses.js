517843967206d3697fa722dbd2e9294b7826fbff
JoyDore63 <joy.dore@isotoma.com>
2016-02-25 10:03:08 +0000

    There or thereabouts

--- expenses_website/expenses/static/expenses/js/expenses.js
+++ expenses_website/expenses/static/expenses/js/expenses.js
@@ -1,36 +1,51 @@
 $(document).ready(function(){
-    $('#id_category').on('change', function(){
+    // for each of the input fields,
+    //   if value was entered
+    //     set to valid and disable error message
+    //   else (no value has been input)
+    //     set error message to show and flag as invalid
+    $('#id_category').on('input', function(){
         var input=$(this);
         var value=input.val();
+        var error_element=$("span", $(this).parent());
         if (value){
             input.removeClass("invalid").addClass("valid");
+            error_element.removeClass("error_show").addClass("error");
         }
         else {
             input.removeClass("valid").addClass("invalid");
+            error_element.removeClass("error").addClass("error_show");
         }
     });
     $('#id_description').on('input', function(){
         var input=$(this);
         var value=input.val();
+        var error_element=$("span", $(this).parent());
         if (value){
             input.removeClass("invalid").addClass("valid");
+            error_element.removeClass("error_show").addClass("error");
         }
         else {
             input.removeClass("valid").addClass("invalid");
+            error_element.removeClass("error").addClass("error_show");
         }
     });
     $("#id_price").on('input', function() {
         var input=$(this);
         var value=input.val();
+        var error_element=$("span", $(this).parent());
         if (value){
             input.removeClass("invalid").addClass("valid");
+            error_element.removeClass("error_show").addClass("error");
         }
         else {
             input.removeClass("valid").addClass("invalid");
+            error_element.removeClass("error").addClass("error_show");
         }
     });
     // taken from https://formden.com/blog/validate-contact-form-jquery
-    $("#expense_submit").click(function(event){
+    // If the form changes, check if we can enable submit button
+    $("#expense").change(function(event){
         // Don't need to validate the date fields, they have defaults
         $("#id_purchase_date_month").addClass("valid");
         $("#id_purchase_date_day").addClass("valid");
@@ -43,22 +58,19 @@ $(document).ready(function(){
         var error_free=true;
         for (var input in form_data){
             var element=$("#id_"+form_data[input]['name']);
-            // Ignore csrf token, all attempts at adding valid to it have failed
+            // Ignore csrf token, attempts at adding valid to it have failed
             if (element.selector == '#id_csrfmiddlewaretoken'){continue;}
             var valid=element.hasClass("valid");
             var error_element=$("span", element.parent());
             if (!valid){
-                error_element.removeClass("error").addClass("error_show");
                 error_free=false;
             }
-            else{error_element.removeClass("error_show").addClass("error");}
         }
         if (!error_free){
-            event.preventDefault();
-            alert("Errors: Form will not be submitted")
+            $("#expense_submit").addClass("disabled");
         }
         else{
-            alert('No errors: Form will be submitted');
+            $('#expense_submit').removeClass("disabled");
         }
     });
 });
